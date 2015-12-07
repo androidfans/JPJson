5a1fa0fbf47465a695cb6938e06fdf114a5534fc
liuli <2458853753@qq.com>
2015-12-07 15:25:09 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JPJson
 JPJson is a library to deal with Json data in Java supported by JParsec.
+
+
+当不知道接口怎么设计,无从下手的时候,可以先写写测试.所有的目标就会变得清晰
---
+++ src/com/ll/JPJson/test/TestToClarify.java
@@ -0,0 +1,14 @@
+package com.ll.JPJson.test;
+
+/**
+ * Created by liuli on 15-12-7.
+ */
+
+public class TestToClarify {
+/*
+    String json = "{"name" : "liuli"}";
+    JsonElement obj = JPJson.parse(json);
+    JsonElement q = obj.get("name");
+
+*/
+}
